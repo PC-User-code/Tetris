b5c577eeeef010cc86fbe76c5de84177394c2233
PC_user <ovcchinnikovdenchik@gmail.com>
2024-01-02 14:12:19 +0300

    line_on_blue_fon

--- Main.py
+++ Main.py
@@ -1,14 +1,24 @@
 import pygame
 import os
 import sys
-        #test_GIT_connect
 
-settings = [816, 816, 32, 60]           # width, height, size_title, fps
+settings = [816, 816, 32]               # width, height, size_title
+FPS = 60
 
 class Tetris:
     def __init__(self):
-        figures = ["T", "L", "S", "Lr", "I"]    # возможные фигруы в тетрисе (игра создана изначально русским программистом на языке Pascal
-                                                # ее название символично ведь каждая фигура состоит из 4 кваратов)
+        figures = ["O", "T", "L", "S", "J", "I", "Z"]    # возможные фигруы в тетрисе (игра создана изначально русским программистом на языке Pascal
+                                                # ее название символично ведь каждая фигура состоит из 4 кваратов\
+
+class Interface(pygame.sprite.Sprite):
+    def __init__(self):             #отрисовка интерфейса при игре
+            super().__init__()
+            self.image = pygame.Surface([width, height])
+            self.image.fill(pygame.Color("#38B2CE"))
+            self.rect = self.image.get_rect()
+            self.rect.x, self.rect.y = 1, 1
+            pygame.draw.line(self.image, pygame.Color("white"), (0, 0), (50, 50))
+
 
 if __name__ == "__main__":
     pygame.init()
@@ -17,21 +27,18 @@ if __name__ == "__main__":
     pygame.display.set_icon(icon)
     size = width, height = settings[0], settings[1]
     tile_size = settings[2]
-    FPS = settings[3]
-    ui_sprite = pygame.sprite.Group()
-    #надо сделать спрайт, как на картинке в папке data dendy.gif
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     all_sprites = pygame.sprite.Group()
+    gi = Interface()
+    all_sprites.add(gi)
     running = True
-
     while running:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 running = False
         screen.fill((0, 0, 0))
-        all_sprites.update(events)
         all_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
